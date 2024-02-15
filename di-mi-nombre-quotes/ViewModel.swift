67594a6802f095c8b048e5629cd9f38102114e82
= <=>
2024-02-14 21:39:45 -0600

    Comments added for the ViewModel

--- di-mi-nombre-quotes/ViewModel.swift
+++ di-mi-nombre-quotes/ViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 @MainActor
 class ViewModel: ObservableObject{
+    // Define the Status enum
     enum Status {
         case notStarted
         case fetching
@@ -16,22 +17,29 @@ class ViewModel: ObservableObject{
         case failed(error: Error)
     }
     
+    // By default initialice with .notStarted value
     @Published private(set) var status: Status = .notStarted
     
+    // Create controller from FetchController class
     private let controller: FetchController
     
     init(controller: FetchController){
         self.controller = controller
     }
     
+    // Fetch the data of the quote and character from the controller
     func getData(for show: String) async{
+        // Starts the process of fetching the data
         status = .fetching
         
         do{
+            // Gets the quote of specific show
             let quote = try await controller.fetchQuote(from: show)
             
+            // Gets the character of specified character name
             let character = try await controller.fetchCharacter(quote.character)
             
+            // Changes the status enum for a success state
             status = .success(data: (quote, character))
         } catch{
             status = .failed(error: error)
