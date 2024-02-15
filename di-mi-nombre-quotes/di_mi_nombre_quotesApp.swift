9d996444311891b10e40ddf260439c936198bcd1
= <=>
2024-02-14 20:32:37 -0600

    Initial Commit

---
+++ di-mi-nombre-quotes/di_mi_nombre_quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  di_mi_nombre_quotesApp.swift
+//  di-mi-nombre-quotes
+//
+//  Created by Manolo on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct di_mi_nombre_quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
