0a3beee0ef44cfe19989ed9b35039aa9cb6188cd
= <=>
2024-02-14 20:38:30 -0600

    Add file Quote

---
+++ di-mi-nombre-quotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  di-mi-nombre-quotes
+//
+//  Created by Manolo on 14/02/24.
+//
+
+import Foundation
+
+struct Quote: Decodable{
+    let quote: String
+    let character: String
+    let production: String
+}
