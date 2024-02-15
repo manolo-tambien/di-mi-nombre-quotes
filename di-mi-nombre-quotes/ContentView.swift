26a5c11336463227c88334176c715433afe406c4
= <=>
2024-02-14 21:10:46 -0600

    Comments added for the ContentView view

--- di-mi-nombre-quotes/ContentView.swift
+++ di-mi-nombre-quotes/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
+            // Render the QuoteView view sending the values for breaking bad tab
             QuoteView(show: Constants.bbName)
                 .tabItem {
+                    // Set the name of the tab and the icon
                     Label(Constants.bbName, systemImage: "tortoise")
                 }
+            // Render the QuoteView view sending the value for a Better call saul tab
             QuoteView(show: Constants.bcsName)
                 .tabItem {
                     Label(Constants.bcsName, systemImage: "briefcase")
